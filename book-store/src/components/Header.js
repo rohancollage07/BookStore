ed79aed3b2144d15bcbdf67d9873b18d069761ba
Rohan Dongre <rohancollege07@gmail.com>
2023-08-28 14:37:03 +0530

    home edit started

--- book-store/src/components/Header.js
+++ book-store/src/components/Header.js
@@ -1,64 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import { Typography, Tabs, Tab } from '@mui/material'
-import AutoStoriesIcon from '@mui/icons-material/AutoStories'
-import { NavLink } from 'react-router-dom'
-import Sidebar from './Sidebar'
-import './Main.css'
+import React, { useState, useEffect } from 'react';
+import { Typography, Tabs, Tab } from '@mui/material';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import { NavLink } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import './Main.css';
 
 
 const Header = () => {
-  
-  const [isSmallScreen, setIsSmallScreen] = useState(false)
-  const [color, setColor] = useState('#C2A792')
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
 
-  // Use Effect to handle screen size small to show menu icon
   useEffect(() => {
     const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 786)
-    }
+      setIsSmallScreen(window.innerWidth < 786);
+    };
 
-    // Initial check on component mount
-    handleResize()
+    handleResize();
+    window.addEventListener('resize', handleResize);
 
-    // Attach event listener for window resize
-    window.addEventListener('resize', handleResize)
-
-    // Clean up the event listener on component unmount
     return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
 
   return (
     <div>
-
-          {!isSmallScreen ? ( 
-            // Only show tabs on larger screens
-            <Tabs
-            className='Tabs_Header'
-            centered
-            value={color}
-            onChange={() => setColor('#2C1810')}
-            >
-              <Tab className='Tab_Individual' LinkComponent={NavLink} to="/add" label="Add product" />
-              <Tab className='Tab_Individual' LinkComponent={NavLink} to="/books" label="Books" />
-              <NavLink to="/" >
-          <Typography  >
-              <AutoStoriesIcon sx={{ fontSize: '3rem' }} />
+      {!isSmallScreen ? (
+        <Tabs
+          className='Tabs_Header'
+          centered
+          // value={activeTab}
+          onChange={handleTabChange}
+        >
+          <Tab
+            className={`Tab_Individual ${activeTab === 0 ? 'active' : ''}`}
+            component={NavLink}
+            to="/add"
+            label="Add product"
+          />
+          <Tab
+            className={`Tab_Individual ${activeTab === 1 ? 'active' : ''}`}
+            component={NavLink}
+            to="/books"
+            label="Books"
+          />
+          <NavLink to="/" onClick={setActiveTab} >
+            <Typography>
+              <AutoStoriesIcon sx={{ fontSize: '3rem', color: '#C2A792' }} />
             </Typography>
           </NavLink>
-              <Tab className='Tab_Individual'  LinkComponent={NavLink} to="/cart" label="Cart" />
-              <Tab className='Tab_Individual'  LinkComponent={NavLink} to="/about" label="About Us" />
-            </Tabs>
-          ) : (
-            // Show sidebar icon on smaller screens
-            
-            <Sidebar />
-          )}
-        
-      
+          <Tab
+            className={`Tab_Individual ${activeTab === 3 ? 'active' : ''}`}
+            component={NavLink}
+            to="/cart"
+            label="Cart"
+          />
+          <Tab
+            className={`Tab_Individual ${activeTab === 4 ? 'active' : ''}`}
+            component={NavLink}
+            to="/about"
+            label="About Us"
+          />
+        </Tabs>
+      ) : (
+        <Sidebar />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
